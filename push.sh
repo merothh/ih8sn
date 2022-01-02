9b067e30f565b5217425e0af504e10c3e8d27c37
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-02 16:33:12 +0100

    Move non-code to etc/
    Change-Id: I815e591f6fed4b233349ae10fc631b8997d2bf34
    Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- push.sh
+++ push.sh
@@ -2,17 +2,17 @@
 
 adb wait-for-device root
 adb wait-for-device remount
-adb wait-for-device push 60-ih8sn.sh /system/addon.d/
+adb wait-for-device push etc/60-ih8sn.sh /system/addon.d/
 adb wait-for-device push ih8sn /system/bin/
-adb wait-for-device push ih8sn.rc /system/etc/init/
+adb wait-for-device push etc/ih8sn.rc /system/etc/init/
 
 SERIALNO=$(adb shell getprop ro.boot.serialno)
 PRODUCT=$(adb shell getprop ro.build.product)
 
 if [[ -f "ih8sn.conf.${SERIALNO}" ]]; then
-    adb wait-for-device push ih8sn.conf.${SERIALNO} /system/etc/ih8sn.conf
+    adb wait-for-device push etc/ih8sn.conf.${SERIALNO} /system/etc/ih8sn.conf
 elif [[ -f "ih8sn.conf.${PRODUCT}" ]]; then
-    adb wait-for-device push ih8sn.conf.${PRODUCT} /system/etc/ih8sn.conf
+    adb wait-for-device push etc/ih8sn.conf.${PRODUCT} /system/etc/ih8sn.conf
 else
-    adb wait-for-device push ih8sn.conf /system/etc/
+    adb wait-for-device push etc/ih8sn.conf /system/etc/
 fi
